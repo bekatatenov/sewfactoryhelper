76874714dbd7a929d3b7cede8c72ae3d69718667
btatenov <btatenov@nurtelecom.kg>
2022-10-29 19:32:50 +0600

    Initial commit

---
+++ src/main/java/com/sewfactoryhelper/sewfactoryhelper/SewfactoryhelperApplication.java
@@ -0,0 +1,13 @@
+package com.sewfactoryhelper.sewfactoryhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewfactoryhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SewfactoryhelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sewfactoryhelper/sewfactoryhelper/SewfactoryhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sewfactoryhelper.sewfactoryhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewfactoryhelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
