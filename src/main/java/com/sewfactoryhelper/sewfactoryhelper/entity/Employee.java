64560af5ae47d95b234bca448197d4b3333a7761
brutender <fakedere@gmail.com>
2022-11-22 14:56:28 +0600

    in proccess employee and fabric

--- src/main/java/com/sewfactoryhelper/sewfactoryhelper/entity/Employee.java
+++ src/main/java/com/sewfactoryhelper/sewfactoryhelper/entity/Employee.java
@@ -1,4 +1,35 @@
 package com.sewfactoryhelper.sewfactoryhelper.entity;
 
+
+import com.sewfactoryhelper.sewfactoryhelper.enums.Product;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Employee extends BaseEntity{
+
+    @Enumerated (EnumType.STRING)
+    Product product;
+
+    @Column (name = "qty", nullable = false)
+    Integer qty;
+
+    //@ManyToMany
+    //@JoinColumn (name = "price_id", nullable = false)
+    //Salary salary;
+
+    @Column (name = "reject")
+    Integer reject;
+
+    @Column (name = "salary")
+    Integer salary
 }
